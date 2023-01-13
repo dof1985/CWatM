84523c4ebbab92fab0e358e4d37f532125a2cb07
f-s-d <98038994+f-s-d@users.noreply.github.com>
2023-01-13 11:23:10 +0100

    as per request

--- cwatm/cwatm_initial.py
+++ cwatm/cwatm_initial.py
@@ -1,7 +1,6 @@
 # -------------------------------------------------------------------------
 # Name:       CWATM Initial
 # Purpose: 
-# another line2
 # Author:      PB
 #
 # Created:     16/05/2016
