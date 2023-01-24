474246e0c96ec5d6f552f6376345168376de0b37
f-s-d <98038994+f-s-d@users.noreply.github.com>
2023-01-24 13:37:20 +0100

    Connection of Erosion-sediment script (erosed.py) to waterquality_main.py
    I have created erosed.py and connected it to waterquality_main.py.
    
    I tested for every combination of "includeWaterquality" in options and "includeErosed" in bindings, by printing out test messages.

--- cwatm/hydrological_modules/water_quality/waterquality_main.py
+++ cwatm/hydrological_modules/water_quality/waterquality_main.py
@@ -12,8 +12,9 @@
 from cwatm.management_modules.data_handling import *
 from cwatm.management_modules.globals import *
 
-# define water quality sub-modules
+# import water quality sub-modules
 from cwatm.hydrological_modules.water_quality.phosphorus import waterquality_phosphorus
+from cwatm.hydrological_modules.water_quality.erosed import waterquality_erosed
 
 class water_quality(object):
     """
@@ -45,9 +46,10 @@ class water_quality(object):
         
         # define water quality sub-modules
         self.waterquality_p = waterquality_phosphorus(self)
+        self.erosed = waterquality_erosed(model)
 	
     def initial(self):
-        
+        """
         # soil calculation - e.g., water quality soil layers
         # soil depths for water quality
         # soil masses for water quality
@@ -57,27 +59,38 @@ class water_quality(object):
         
         # dynamic -> load inputs and ground cover adjustment
         # dynamic_soil, dynamic_...
-        
-        # check if water quality is included
+        """
+
+        # check settings file, if water quality should be included
         self.var.includeWaterQuality =  False
         if 'includeWaterQuality' in option:
             self.var.includeWaterQuality =  checkOption('includeWaterQuality')
 
         if self.var.includeWaterQuality:
             # load sub-modules
-             
             self.var.includePhosphorus = False
             if 'includePhosphorus' in binding:
                 self.var.includePhosphorus = returnBool('includePhosphorus')
+
+            self.var.includeErosed = False
+            if 'includeErosed' in binding:
+                self.var.includeErosed = returnBool('includeErosed')
             
             
             # run initial sub-modules
             
             if self.var.includePhosphorus:
                 self.waterquality_p.initial()
+
+            if self.var.includeErosed:
+                self.erosed.initial()
         
             
     def dynamic(self):
-        print("wq dyn.")
-    
-       
+
+        if self.var.includeWaterQuality:
+            if self.var.includeErosed:
+                self.erosed.dynamic()
+
+
+
