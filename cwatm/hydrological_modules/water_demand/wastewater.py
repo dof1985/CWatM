34a1529b1aa880bae98ed8dd4ffd1c52115724f8
Dor Fridman <dof1985@gmail.com>
2024-02-09 10:30:31 +0100

    Update wastewater extensive threshold

--- cwatm/hydrological_modules/water_demand/wastewater.py
+++ cwatm/hydrological_modules/water_demand/wastewater.py
@@ -295,7 +295,7 @@ class waterdemand_wastewater(object):
         
         ### Extensive WWTP ####
         # Identify extensive systems - if timeLag >= 5
-        self.var.extensive = self.var.wwtTimeC >= 5
+        self.var.extensive = self.var.wwtTimeC >= 2
         # number of days to fill a treatement pool in extensive systems - default no. of pools: 3
         self.var.noPools_extensive = 3
         
